dcf4fe84d513ec907ae61524efb755d35b11f144
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2024-06-03 11:46:01 +0530

    Added get all cities api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const {CityService}=require('../services/index');
 const cityService=new CityService();
 
@@ -88,9 +89,34 @@ const update= async(req,res)=>{
     }
 
 }
+const getAll= async(req,res)=>{
+    try {
+        const cities= await cityService.getAllCities();
+        return res.status(200).json({
+            data:cities,
+            success:true,
+            message:"Successfully fetched all cities"
+        });
+    
+        
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to update a city',
+            err:error
+        })
+        
+    }
+
+}
+
 module.exports={
     create,
     destroy,
     get,
-    update
+    update,
+    getAll
 }
