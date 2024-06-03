1875581246e108b1904286ad4314a6bdf518604f
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2024-06-03 16:51:47 +0530

    Added Flight Cruds

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const CityController = require('../../controllers/city-controller');
+const FlightController = require('../../controllers/flight-controller');
 
 const router = express.Router();
 router.post('/city', CityController.create);
@@ -8,6 +9,10 @@ router.delete('/city/:id', CityController.destroy);
 router.get('/city/:id', CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
+router.post('/flights',FlightController.create);
+router.get('/flights', FlightController.getAll);
+router.get('/flights/:id', FlightController.get);
+router.patch('/flights/:id', FlightController.update);
 
 
 module.exports = router;
