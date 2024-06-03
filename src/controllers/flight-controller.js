cc940c88753abf0f17b80d3d5009af95ddbb5b58
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2024-06-03 21:17:21 +0530

    Added fligh-create request middlewares

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,13 +1,21 @@
 const {FlightService} = require('../services/index');
-
+const { SuccessCodes } = require('../utils/error-codes');
 
 const flightService = new FlightService();
 
 const create = async (req, res) => {
     try {
-       
-        const flight = await flightService.createFlight(req,res);
-        return res.status(201).json({
+        const flightRequestData = {
+            flightNumber: req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId: req.body.departureAirportId,
+            arrivalAirportId: req.body.arrivalAirportId,
+            arrivalTime: req.body.arrivalTime,
+            departureTime: req.body.departureTime,
+            price: req.body.price
+        }
+        const flight = await flightService.createFlight(flightRequestData);
+        return res.status(SuccessCodes.CREATED).json({
             data: flight,
             success: true,
             err: {},
